42c91c015359ab98f710653d02196599cc58e7e6
coreym <coreym@vidulum.org>
2018-07-16 04:49:10 +0900

    init

---
+++ include/librustvidulum.h
@@ -0,0 +1,10 @@
+#ifndef LIBRUSTVIDULUM_INCLUDE_H_
+#define LIBRUSTVIDULUM_INCLUDE_H_
+
+#include <stdint.h>
+
+extern "C" {
+    uint64_t librustvidulum_xor(uint64_t a, uint64_t b);
+}
+
+#endif // LIBRUSTVIDULUM_INCLUDE_H_
